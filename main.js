ea8d2a8cdc4f033b8edac4087f84fa7eb4bbe15a
Rezaprtmm <rizki.pratama@students.paramadina.ac.id>
2022-06-13 21:57:24 +0700

    perbaikan dan animasi

---
+++ main.js
@@ -0,0 +1,23 @@
+(function() {
+"use strict";
+
+gsap.registerPlugin(TextPlugin);
+gsap.to('.lead', { duration: 4, delay: 0.5, text: 'Hey, We Are<br>Paramadina<br>Entrepreneur Club'})
+gsap.from('.button-hero', { duration: 3, x: -100, opacity: 0, delay: 2, ease: 'back' })
+gsap.to('.waiting-join', { duration: 4, delay: 0.5, text: 'What are u waiting for?<br>Let \'s join us'})
+
+
+const activitiesImage = document.querySelectorAll('.activities-card');
+
+activitiesImage.forEach((img, i) => {
+img.dataset.aos = 'flip-left';
+img.dataset.aosDelay = i * 100;
+img.dataset.aosDuration = 1000;
+});
+
+AOS.init({
+    duration: 2000    
+  });
+
+    
+})()
