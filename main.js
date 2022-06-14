5d05f598ceac2bcdca1b957bd33743aa751cf2b0
Rezaprtmm <rizki.pratama@students.paramadina.ac.id>
2022-06-14 21:47:32 +0700

    edit animasi

--- main.js
+++ main.js
@@ -4,7 +4,11 @@
 gsap.registerPlugin(TextPlugin);
 gsap.to('.lead', { duration: 4, delay: 0.5, text: 'Hey, We Are<br>Paramadina<br>Entrepreneur Club'})
 gsap.from('.button-hero', { duration: 3, x: -100, opacity: 0, delay: 2, ease: 'back' })
-gsap.to('.waiting-join', { duration: 4, delay: 0.5, text: 'What are u waiting for?<br>Let \'s join us'})
+ window.addEventListener('scroll', function() {
+  gsap.to('.waiting-join', { duration: 3, delay: 0.5, text: 'What are u waiting for?<br>Let \'s join us'});
+
+});
+
 
 
 const activitiesImage = document.querySelectorAll('.activities-card');
@@ -14,10 +18,19 @@ img.dataset.aos = 'flip-left';
 img.dataset.aosDelay = i * 100;
 img.dataset.aosDuration = 1000;
 });
+const programImage = document.querySelectorAll('.program-card');
+
+programImage.forEach((img, i) => {
+img.dataset.aos = 'flip-left';
+img.dataset.aosDelay = i * 100;
+img.dataset.aosDuration = 1000;
+});
+
 
 AOS.init({
     duration: 2000    
   });
+
 
     
 })()
